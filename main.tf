09d07d2bdc27c35e50a93a203dcbd3375bc9a953
Jose Sebastian <josesebn@gmail.com>
2021-07-14 01:58:49 -0400

    skip

--- main.tf
+++ main.tf
@@ -1,12 +1,8 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIAVWUN5TJ544FJE2KT"
-  secret_key = "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"
+  access_key = "AKIAVWUN5TJ5U2TN4K4Q"
+  secret_key = "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"
 }
-
-//variable "subnet_prefix" {
-//  description = "cidr block for the subnet"
-//}
 
 resource "aws_vpc" "webserver-vpc" {
   cidr_block = "10.0.0.0/16"
@@ -140,48 +136,22 @@ resource "aws_elb" "elbWeb" {
 //  public_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCFk5r9ZHrdeyIwMr+UL4m34SbsP5vItImPB947I+8L/ZJv6h0XgBkGvVODoJ/uSo3is1z0LkKvPR7nb/wq9lUdGFYKS0cCksK5bwv/WvvNbdvF9ZzRAfFWtUe3AHoxpGtW9jC5AroMDu3UVsGHLrT8AxxaNbYRcVEiK9lboet6Ri3hA5d1OBlFZrLbfXY1EwmBd5McCclNecKQQVjNRQWK0C1BnRtM3OaUHVlKShQzR+1kuPgwTnDog4aSDsZBiXQ57FDC5tBRlmDSoj/fWit9pPps1B5VP8jf3uLvNhG+nj8derWcU39oBdagGgGe2WAF0j8yt1dlUXtkYZQ7PIj5"
 //}
 
-  #Create a network interface with an ip in the subnet that was created in step 4
-//resource "aws_network_interface" "webserver-nic" {
-//   subnet_id       = aws_subnet.subnet-1.id
-//   private_ips     = ["10.0.1.50"]
-//   security_groups = [aws_security_group.allow_web.id]
-//
-// }
-
-#  Assign an elastic IP to the network interface created
-
-//resource "aws_eip" "eip" {
-//   vpc                       = true
-//   network_interface         = aws_network_interface.webserver-nic.id
-//   associate_with_private_ip = "10.0.1.50"
-//  depends_on                = [aws_internet_gateway.igw]
-// }
-
 
 #Create Ubuntu server and install/enable nginx
 resource "aws_instance" "web_server_instance" {
-  ami = "ami-0db72f413fc1ddb2a"
+  ami = "ami-0dc2d3e4c0f9ebd18"
   instance_type = "t2.nano"
   availability_zone = "us-east-1a"
   subnet_id = aws_subnet.subnet-1.id
   vpc_security_group_ids = [aws_security_group.allow_web.id]
   key_name          = "my-key"
   associate_public_ip_address = true
-
-  //     network_interface {
-//       device_index         = 0
-//       network_interface_id = aws_network_interface.webserver-nic.id
-//     }
-//  connection {
-//    # The default username for our AMI
-//    user = "ubuntu"
-//  }
   user_data = <<-EOF
                  #!/bin/bash
                  sudo apt update -y
-                 amazon-linux-extras install nginx1 -y
+                 sudo yum install docker -y
+                 sudo amazon-linux-extras install nginx1 -y
                  sudo service nginx start
-                 aws s3 cp s3://jose33/version.txt version.txt
                  EOF
  // user_data = file("./entrypoint.sh")
   tags = {
